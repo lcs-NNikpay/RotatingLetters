1fd11590fca4ab3ccb763fd6ed5a6bfb77dc423d
Nicki Nikpay <nnikpay@lcs-mba3214.lcs.on.ca>
2018-04-10 15:22:55 -0400

    We added the third part and also made givenInput usable outside the scope.

--- RotatingLetters/main.swift
+++ RotatingLetters/main.swift
@@ -11,7 +11,8 @@ import Foundation
 // INPUT
 
 // Loop forever untill we get good input
-while 1 == 1 {
+var validInput = " " // outside the loop -- we can use it outside the loop
+prompt: while 1 == 1 {
    
     // Prompt the user
     print ("Input word is?")
@@ -33,11 +34,34 @@ while 1 == 1 {
         continue // PROMPT AGAIN
         
     }
-
+    // Test #3 : Check to ensure there is only UPERCASE letters and no spaces
+    checking :  for individualCharacter in givenInput {
+        
+        // Debug: Print the character we are looking at
+        print(individualCharacter)
+        
+        // Check the character
+        switch individualCharacter {
+        case  "A", "B", "C", "D", "E", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q",
+              "R", "S", "T","U","V","W","X","Y","Z" :
+            continue checking // OK, GO TO THE NEX CHARACTER IN THE STRING
+        default:
+            continue prompt // Stop looking, we've found bad input
+        }
+    }
     
     // If we got to this point, we know the input is good
+    validInput = givenInput
     break // quit the while loop and begin PROCESS section
+    
+   
+    
+    
+    
 }
+
+//Progress
+print (validInput)
 
 
 
